36b3c2cd42f4dc58a4bb457bc3533d94ab4b6c8a
Abdul Ragib <abdulragib36@gmail.com>
2023-06-08 01:33:45 +0530

    using payload

--- index.js
+++ index.js
@@ -4,15 +4,24 @@ import logger from 'redux-logger';
 const store=createStore(reducer,applyMiddleware(logger.default));
 const history=[]
 
-function reducer(state={amount:1},action){
+function reducer(state={amount:5},action){
     
    if(action.type==='increment')
    {
         return {amount:state.amount+1};
    }
-   else{
-      return state;
+
+   if(action.type==='decrement')
+   {
+        return {amount:state.amount-1};
    }
+
+   if(action.type==='incrementByAmount')
+   {
+       return {amount : state.amount + action.payload}
+   }
+   
+   return state;
 }
 
 store.subscribe(()=>{
@@ -21,6 +30,6 @@ store.subscribe(()=>{
 })
 
 setInterval(()=>{
-   store.dispatch({type:"increment"})
+   store.dispatch({type:"incrementByAmount",payload:4})
 },5000)
 
