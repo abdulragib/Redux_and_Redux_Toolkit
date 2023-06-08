0e76fe8fa8178292aee5842f0fb8761bcf10f276
Abdul Ragib <abdulragib36@gmail.com>
2023-06-08 08:43:36 +0530

    api error handling and dispatch action according to api response state

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import { createStore, applyMiddleware,combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import logger from "redux-logger";
 import axios from "axios";
 import thunk from "redux-thunk";
 
 //action name constant
-const init = "account/init";
+// const init = "account/init";
 const increment = "account/increment";
 const decrement = "account/decrement";
 const incrementByAmount = "account/incrementByAmount";
-const incrementBonus="bonus/increment"
+const incrementBonus = "bonus/increment";
+const getAccUserFullFilled = "account/getUser/fullfilled";
+const getAccUserRejected = "account/getUser/rejected";
+const getAccUserPending = "account/getUser/pending";
 
 const store = createStore(
   combineReducers({
-  account:accountReducer,
-  bonus:bonusReducer,
-}),
+    account: accountReducer,
+    bonus: bonusReducer,
+  }),
   applyMiddleware(logger.default, thunk.default)
 );
 
@@ -22,8 +25,14 @@ const history = [];
 
 function accountReducer(state = { amount: 5 }, action) {
   switch (action.type) {
-    case init:
-      return { amount: action.payload };
+    case getAccUserFullFilled:
+      return { amount: action.payload, pending:false };
+
+    case getAccUserPending:
+      return { ...state, pending:true };
+
+    case getAccUserRejected:
+      return { ...state, error: action.error , pending:false};
 
     case increment:
       return { amount: state.amount + 1 };
@@ -39,60 +48,66 @@ function accountReducer(state = { amount: 5 }, action) {
   }
 }
 
+function bonusReducer(state = { points: 0 }, action) {
+  switch (action.type) {
+    case incrementBonus:
+      return { points: state.points + 1 };
 
-function bonusReducer(state={points:0},action)
-{
-    switch(action.type)
-    {
-        case incrementBonus:
-            return {points:state.points+1};
+    case incrementByAmount:
+      if (action.payload >= 100) {
+        return { points: state.points + 1 };
+      }
 
-        case incrementByAmount:
-            if(action.payload>=100)
-            {
-                return {points:state.points+1};
-            }
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
 
 //Action Creator
-function incrementValue()
-{
-    return {type:increment};
+function incrementValue() {
+  return { type: increment };
 }
 
-function decrementValue()
-{
-    return {type:decrement}
+function decrementValue() {
+  return { type: decrement };
 }
 
-function incrementByAmountValue(value)
-{
-    return {type:incrementByAmount,payload:value};
+function incrementByAmountValue(value) {
+  return { type: incrementByAmount, payload: value };
 }
 
-function incrementBonusValue(value){
-    return{type:incrementBonus, payload:value};
+function incrementBonusValue(value) {
+  return { type: incrementBonus, payload: value };
+}
+
+function getAccountUserFullFilled(value) {
+  return { type: getAccUserFullFilled, payload: value };
+}
+
+function getAccountUserRejected(error) {
+  return { type: getAccUserRejected, error: error };
+}
+
+function getAccountUserPending() {
+  return { type: getAccUserPending };
 }
 
 //thunk middleware
- function getUser(id) {
+function getUserAccount(id) {
   return async (dispatch, getState) => {
-    const { data } = await axios.get(`http://localhost:3000/account/${id}`);
-    dispatch(initUser(data.amount));
+    try {
+      dispatch(getAccountUserPending())
+      const { data } = await axios.get(`http://localhost:3000/account/${id}`);
+      dispatch(getAccountUserFullFilled(data.amount));
+    } catch (err) {
+      dispatch(getAccountUserRejected(err.message));
+    }
   };
 }
 //always keep in mind we can only dispatch object to reducer not promises.
 
-function initUser(value) {
-  return { type: init, payload: value };
-}
-
 setTimeout(() => {
-    //store.dispatch(getUser(1));
-//   store.dispatch(incrementByAmountValue(100));
-    store.dispatch(incrementBonusValue(5))
+  store.dispatch(getUserAccount(1));
+  //   store.dispatch(incrementByAmountValue(100));
+  // store.dispatch(incrementBonusValue(5))
 }, 3000);
